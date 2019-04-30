c73f7f7b7a8438545cb6faa5477b9f2966b4cb17
sajrus33 <brianwala22@gmail.com>
2019-04-30 03:13:32 +0200

    ..

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const gulp = require("gulp");
+const concat = require("gulp-concat");
+const cleanCSS = require("gulp-clean-css");
+const watch = require("gulp-watch");
+
+
+
+gulp.task("watch-portfolio", function () {
+    watch(["./src/css/*.css"], () => {
+        return gulp.src(['./src/css/variables.css', './src/css/form.css', './src/css/typeForm.css', './src/css/specialistForm.css', './src/css/dateForm.css', './src/css/summaryForm.css', './src/css/detailForm.css', './src/css/media.css'])
+            .pipe(concat("index.css"))
+            .pipe(cleanCSS())
+            .pipe(gulp.dest("./public/css/"));
+    });
+});
+
