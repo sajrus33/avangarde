3fa369f4680eda8c565908455f3ac62cabc6046a
sajrus33 <brianwala22@gmail.com>
2019-05-03 15:52:50 +0200

    ..

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,10 @@
 // progress
 const progressBars = document.querySelectorAll(".progress__bar");
 const progressImgs = document.querySelectorAll(".progress__img");
+
 // all forms
 const forms = document.querySelectorAll(".register__section");
+
 // first form
 const types = document.querySelectorAll(".type__a");
 const typeForm = document.querySelector(".register__section--first");
@@ -11,10 +13,10 @@ types.forEach(type => {
     console.log({ type });
 
     type.addEventListener("click", () => {
-        typeForm.classList.add("displayNone");
-        typeForm.nextElementSibling.classList.remove("displayNone");
+        typeForm.classList.toggle("displayNone");
+        typeForm.nextElementSibling.classList.toggle("displayNone");
     });
-})
+});
 // second form
 const specialists = document.querySelectorAll(".specialist__a");
 const specialistForm = document.querySelector(".register__section--second");
@@ -23,12 +25,22 @@ specialists.forEach(specialist => {
     console.log({ specialist });
 
     specialist.addEventListener("click", () => {
-        specialistForm.classList.add("displayNone");
-        specialistForm.nextElementSibling.classList.remove("displayNone");
+        specialistForm.classList.toggle("displayNone");
+        specialistForm.nextElementSibling.classList.toggle("displayNone");
     });
-})
+});
 
+// thirt form
+const dates = document.querySelectorAll(".date__a");
+const dateForm = document.querySelector(".register__section--thirt");
 
+dates.forEach(date => {
+    console.log({ date });
 
+    date.addEventListener("click", () => {
+        dateForm.classList.toggle("displayNone");
+        dateForm.nextElementSibling.classList.toggle("displayNone");
+    });
+});
 
 console.log({ types });
