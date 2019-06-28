5308c08118409d21cfba39571ed3000cdcbbfd08
sajrus33 <brianwala22@gmail.com>
2019-06-28 17:32:10 +0200

    .

--- public/js/index.js
+++ public/js/index.js
@@ -2,6 +2,17 @@
 const progressBars = document.querySelectorAll(".progress__bar");
 const progressImgs = document.querySelectorAll(".progress__img");
 
+const runProgress = amount => {
+  progressBars.forEach((bar, index) => {
+    if (index === amount) {
+      bar.classList.toggle("progress__bar--active");
+      progressImgs[index].src = "/avangarde/public/img/progress--done--big.png";
+      progressImgs[index].classList.toggle("progress__img--active");
+    }
+  });
+  //   progress--done--big
+};
+
 // all forms
 const forms = document.querySelectorAll(".register__section");
 
@@ -10,24 +21,26 @@ const types = document.querySelectorAll(".type__a");
 const typeForm = document.querySelector(".register__section--first");
 
 types.forEach(type => {
-    // console.log({ type });
+  // console.log({ type });
 
-    type.addEventListener("click", () => {
-        typeForm.classList.toggle("displayNone");
-        typeForm.nextElementSibling.classList.toggle("displayNone");
-    });
+  type.addEventListener("click", () => {
+    typeForm.classList.toggle("displayNone");
+    typeForm.nextElementSibling.classList.toggle("displayNone");
+    runProgress(0);
+  });
 });
 // second form
 const specialists = document.querySelectorAll(".specialist__a");
 const specialistForm = document.querySelector(".register__section--second");
 
 specialists.forEach(specialist => {
-    // console.log({ specialist });
+  // console.log({ specialist });
 
-    specialist.addEventListener("click", () => {
-        specialistForm.classList.toggle("displayNone");
-        specialistForm.nextElementSibling.classList.toggle("displayNone");
-    });
+  specialist.addEventListener("click", () => {
+    specialistForm.classList.toggle("displayNone");
+    specialistForm.nextElementSibling.classList.toggle("displayNone");
+    runProgress(1);
+  });
 });
 
 // thirt form
@@ -35,21 +48,25 @@ const dates = document.querySelectorAll(".date__a");
 const dateForm = document.querySelector(".register__section--thirt");
 
 dates.forEach(date => {
-    // console.log({ date });
+  // console.log({ date });
 
-    date.addEventListener("click", () => {
-        dateForm.classList.toggle("displayNone");
-        dateForm.nextElementSibling.classList.toggle("displayNone");
-    });
+  date.addEventListener("click", () => {
+    dateForm.classList.toggle("displayNone");
+    dateForm.nextElementSibling.classList.toggle("displayNone");
+    runProgress(2);
+  });
 });
-
 
 // fourth form
 const summaryForm = document.querySelector(".register__section--fourth");
 
-const summaryInputCategory = document.querySelector(".summary__input--category");
+const summaryInputCategory = document.querySelector(
+  ".summary__input--category"
+);
 const summaryInputType = document.querySelector(".summary__input--type");
-const summaryInputSpecialist = document.querySelector(".summary__input--specialist");
+const summaryInputSpecialist = document.querySelector(
+  ".summary__input--specialist"
+);
 const summaryInputDate = document.querySelector(".summary__input--date");
 const summaryInputHour = document.querySelector(".summary__input--hour");
 const summaryInputName = document.querySelector(".summary__input--name");
@@ -58,19 +75,17 @@ const summaryInputPhone = document.querySelector(".summary__input--phone");
 const summaryBtnCancel = document.querySelector(".summary__button--cancel");
 const summaryBtnRegister = document.querySelector(".summary__button--register");
 
-
-
 const chwiloweGuziki = document.querySelectorAll(".summary__button");
 
 chwiloweGuziki.forEach(guzik => {
-    guzik.addEventListener("click", () => {
-        summaryForm.classList.toggle("displayNone");
-        summaryForm.nextElementSibling.classList.toggle("displayNone");
-    });
+  guzik.addEventListener("click", () => {
+    summaryForm.classList.toggle("displayNone");
+    summaryForm.nextElementSibling.classList.toggle("displayNone");
+    runProgress(3);
+  });
 });
 
 // console.log({ types });
-
 
 // fifth form
 const detailsForm = document.querySelector(".register__section--fifth");
