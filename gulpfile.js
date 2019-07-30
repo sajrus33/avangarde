37a8107d3f5198a26a8f7f444be73361608ed292
sajrus33 <brianwala22@gmail.com>
2019-07-30 16:46:36 +0200

    package json, gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -6,11 +6,16 @@ const watch = require("gulp-watch");
 
 
 gulp.task("watch", function () {
-    watch(["./src/css/*.css"], () => {
-        return gulp.src(['./src/css/variables.css', './src/css/form.css', './src/css/typeForm.css', './src/css/specialistForm.css', './src/css/dateForm.css', './src/css/summaryForm.css', './src/css/detailForm.css', './src/css/media.css'])
+    watch("./src/css/*.css", () => {
+        return gulp.src(['src/css/variables.css', './src/css/form.css', './src/css/typeForm.css', './src/css/specialistForm.css', './src/css/dateForm.css', './src/css/summaryForm.css', './src/css/detailForm.css', './src/css/media.css'])
             .pipe(concat("index.css"))
             .pipe(cleanCSS())
             .pipe(gulp.dest("./public/css/"));
     });
 });
 
+// gulp.task('watch', () =>{
+
+// gulp.watch(['app/sass/**/*.sass'], gulp.series('sass'));
+
+// });
